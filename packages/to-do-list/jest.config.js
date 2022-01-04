1d25256c34aa4272a888bcd9ae46b3d8e64ca609
Stanisław Uss <uss.stanislaw@protonmail.com>
2022-01-05 00:15:28 +0100

    feat: scaffold next app

---
+++ packages/to-do-list/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'to-do-list',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/packages/to-do-list',
+};
